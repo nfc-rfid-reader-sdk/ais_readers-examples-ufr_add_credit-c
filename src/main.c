eab220d8e4e590865bc6e5c2c5bd087c944f3b6d
developer4 <support@d-logic.rs>
2017-12-01 13:26:09 +0100

    Minor cosmetic changes in main.c

--- src/main.c
+++ src/main.c
@@ -73,14 +73,15 @@ int main(void)
 	status = ReaderReset();
 	if (status != UFR_OK)
 	{
+		ReaderClose();
 		printf("Error while opening device, status is: 0x%08X\n", status);
 		getchar();
 		return EXIT_FAILURE;
 	}
 #if __WIN32 || __WIN64
-			Sleep(500);
+	Sleep(500);
 #else // if linux || __linux__ || __APPLE__
-			usleep(500000);
+	usleep(500000);
 #endif
 
 	if (!CheckDependencies())
